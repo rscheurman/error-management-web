5bdd3a539eb314d4d78e9e582813399b9d5e14f6
rscheurman <riley.scheurman@gmail.com>
2021-04-06 20:35:43 -0600

    Initial commit!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ErrorDashboardComponent } from './pages/error-dashboard/error-dashboard.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ErrorDashboardComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
